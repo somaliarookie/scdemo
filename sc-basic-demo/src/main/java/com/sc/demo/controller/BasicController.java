fde483194dbfe3e98a28647eab3115d1ad3222eb
xubo <xubo@akulaku.com>
2020-12-02 12:27:58 +0800

    add some different request forms demo

--- sc-basic-demo/src/main/java/com/sc/demo/controller/BasicController.java
+++ sc-basic-demo/src/main/java/com/sc/demo/controller/BasicController.java
@@ -2,9 +2,16 @@ package com.sc.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -24,6 +31,77 @@ public class BasicController {
 
 	}
 
+
+	/**
+	 *
+	 * 'content-type' : application/json
+	 *
+	 *  使用 @RequestBody注解标识，使用body-json格式传入参数
+	 *
+	 * curl --location --request POST 'http://localhost:8080/post/json' \
+	 * --header 'Content-Type: application/json' \
+	 * --data-raw '{
+	 *     "name": "xubo",
+	 *     "age": 233
+	 * }'
+	 *
+	 */
+	@PostMapping(value = {"/post/json/requestbody"})
+	public User postJsonWithRequestBody(@RequestBody User u) {
+
+		return  u;
+	}
+
+
+	/**
+	 *
+	 * 'content-type' : application/json
+	 *
+	 *
+	 * 不使用任何注解的参数 -> x-www-form-urlencoded
+	 *
+	 * curl --location --request POST 'http://localhost:8080/post/json/withoutann' \
+	 * --header 'Content-Type: application/x-www-form-urlencoded' \
+	 * --data-urlencode 'name=xubo' \
+	 * --data-urlencode 'age=31'
+	 *
+	 */
+	@PostMapping(value = {"/post/json/withoutann"})
+	public User postJsonWithoutAnnotations( User u) {
+
+		return  u;
+	}
+
+	/**
+	 *
+	 * 'content-type' : application/json
+	 *
+	 *
+	 * 不使用任何注解的参数，默认和@RequestBody一样的效果
+	 * curl --location --request POST 'http://localhost:8080/post/json' \
+	 * --header 'Content-Type: application/json' \
+	 * --data-raw '{
+	 *     "name": "xubo",
+	 *     "age": 233
+	 * }'
+	 *
+	 */
+	@PostMapping(value = {"/post/json/map"})
+	public Map postJsonWithoutAnnotations( @RequestBody Map<String, String> info) {
+
+		return  info;
+	}
+
+
+
+	@RequestMapping(value = {"/post"}, method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public String postTest(Long uid, @RequestParam(value = "ids",required = false) List<Long> ids) {
+
+
+		return "post response here: id:"+uid+" ids:"+ids;
+	}
+
 	/**
 	 * process with custom response time {rt}
 	 *
@@ -34,7 +112,7 @@ public class BasicController {
 	public String getWithLantency(@PathVariable Long rt) {
 
 
-		log.info("response time: {} ",rt);
+		log.info("response time: {} ", rt);
 
 		try {
 			TimeUnit.MILLISECONDS.sleep(rt);   // 1
---
+++ sc-basic-demo/src/main/java/com/sc/demo/controller/User.java
@@ -0,0 +1,19 @@
+package com.sc.demo.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @Auth weile
+ * @Time 2020/12/1 17:44
+ * @Description TODO
+ **/
+
+@AllArgsConstructor
+@Data
+public class User {
+
+
+	public String name;
+	public Integer age;
+}
